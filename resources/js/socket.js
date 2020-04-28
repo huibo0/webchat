5fdd085c160c29f0354e11a2544848ebbe11ce01
huibo1 <huibo1@100tal.com>
2020-04-28 18:32:19 +0800

    修改兼容

--- resources/js/socket.js
+++ resources/js/socket.js
@@ -1,6 +1,8 @@
 // 通过 Socket.io 客户端发起 WebSocket 请求
+
 import io from 'socket.io-client';
-import store from "./store";
-let api_token = store.state.userInfo.token;
-const socket = io('http://docker.laigou.shop/?api_token=' + api_token);
+const socket = io('http://docker.laigou.shop', {
+    path: '/ws',
+    transports: ['websocket']
+});
 export default socket;
